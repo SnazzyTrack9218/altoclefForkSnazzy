595adbfe4b9948f43de5893422797d41f6327e2f
SnazzyTrack9218 <72895116+SnazzyTrack9218@users.noreply.github.com>
2025-04-21 20:43:24 -0400

    Update SurviveCommand.java

--- src/main/java/adris/altoclef/commands/SurviveCommand.java
+++ src/main/java/adris/altoclef/commands/SurviveCommand.java
@@ -3,7 +3,9 @@ package adris.altoclef.commands;
 import adris.altoclef.AltoClef;
 import adris.altoclef.commandsystem.Command;
 import adris.altoclef.commandsystem.ArgParser;
-import adris.altoclef.tasks.survival.SurviveTask;
+import adris.altoclef.tasks.resources.GetItemTask;
+import adris.altoclef.tasks.resources.GetBlockTask;
+import adris.altoclef.tasks.resources.EquipItemTask;
 
 public class SurviveCommand extends Command {
 
@@ -13,6 +15,15 @@ public class SurviveCommand extends Command {
 
     @Override
     protected void call(AltoClef mod, ArgParser parser) {
-        mod.runUserTask(new SurviveTask(), this::finish);
+        // Obtain a diamond chestplate
+        mod.runUserTask(new GetItemTask("diamond_chestplate", 1));
+
+        // Equip the diamond chestplate
+        mod.runUserTask(new EquipItemTask("diamond_chestplate"));
+
+        // Gather 20 wood logs
+        mod.runUserTask(new GetBlockTask("log", 20));
+
+        // Additional tasks can be added here
     }
 }
